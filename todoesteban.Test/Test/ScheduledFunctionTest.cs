ed2e17ccad45df41e5071b7fb4ed8f35ccf0a4e3
MAICOL ESTEBAN HINCAPIE VASQUEZ <maicolhincapie200104@correo.itm.edu.co>
2021-08-21 14:46:02 -0500

    publish function

---
+++ todoesteban.Test/Test/ScheduledFunctionTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using todoesteban.Functions.Functions;
+using todoesteban.Test.Helpers;
+using Xunit;
+
+namespace todoesteban.Test.Test
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_Log_Message()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1.10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+
+        // Act
+        ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
